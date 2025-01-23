2b4b9e84784bae90101bb1624221b42fd2a98a37
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-23 17:43:35 +0530

    js.1

--- practise1.js
+++ practise1.js
@@ -2,5 +2,11 @@ let coursename = "javascript";
 function practisescope(){
 
     let topic = "variables";
-    console.log("coursename:", topic)
+    console.log("coursename:", topic);
 }
+if(true){
+
+    let details = "exploring scopes";
+    console.log(details);
+}
+practisescope();

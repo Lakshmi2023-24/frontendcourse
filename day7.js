4ccfba869c39ca1d6f60229814af2e997ef1d695
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-23 19:02:11 +0530

    thid

--- day7.js
+++ day7.js
@@ -1,5 +1,6 @@
-const movie1{
-    name : "murari";
-    price : 150;
-    language : "telugu";
+const movie1 = {
+    name : "murari",
+    AudioProcessingEvent : 150,
+    language : "telugu",
  };
+ console.log(movie1)

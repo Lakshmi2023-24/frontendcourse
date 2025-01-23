bf6f49d8f2e462b8937ad73499825a31e3b8f274
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-23 19:28:31 +0530

    you

--- day7.js
+++ day7.js
@@ -3,4 +3,16 @@ const movie1 = {
     price : 150,
     language : "telugu",
  };
- console.log(movie1)
+ console.log(movie1); 
+ const movie2 = {
+    name : "batsman",
+    price : 100,
+    language : "hindi",
+ }
+ console.log(movie2);
+const movie3 = {
+    name : "avatar",
+    price : 100,
+    language :"english"
+}
+console.log(movie3);

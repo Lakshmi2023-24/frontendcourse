188037613199e37f8409ff17e528e106a3a97889
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-23 10:21:35 +0530

    added

---
+++ let.js
@@ -0,0 +1,8 @@
+let yourname = "lakshmi";
+let yourage = 26;
+let istudent = "true";
+const graduationyear = 2025
+console.log("name:",yourname);
+console.log("yourage:",yourage);
+console.log("studentstatus:",istudent);
+console.log("grauationyear:",graduationyear);
---
+++ task.2.js
@@ -0,0 +1,9 @@
+const yourname = "lakshmi";
+const yourage = 26;
+const istudent = "true";
+const graduationyear = 2025
+console.log("name:",yourname);
+console.log("yourage:",yourage);
+console.log("studentstatus:",istudent);
+console.log("grauationyear:",graduationyear);
+

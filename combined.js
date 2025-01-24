b6d402995bf655d43a58d93c3c408dbc50fb404b
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-24 21:44:13 +0530

    give,js

---
+++ combined.js
@@ -0,0 +1,32 @@
+const students = [
+    {
+        name: "siva kumari",
+        age: 26,
+        gender: "f",
+        bloodgroup: "o+",
+        phoneno: 8787776655,
+    },
+    {
+        name: "arjun",
+        age: 21,
+        gender: "m",
+        bloodgroup: "o-"
+        , phoneno: 8765554442,
+    },
+    {
+        name: "akruthi",
+        age: 24,
+        gender: "f",
+        bloodgroup: "ab=",
+        phoneno: 8765544554,
+    }
+]
+console.log(students)
+console.log(students[0], bloodgroup);
+
+
+
+
+
+
+

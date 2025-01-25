3b8cdbf8eb1807a5e4ae5d309515357e3245f6bf
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-25 19:11:45 +0530

    thin

---
+++ cp.js
@@ -0,0 +1,9 @@
+const student = [
+    {name:"siva",age:24,bloodgroup:"AB-ve",address : "chennai"},
+    {name:"rani",age:23,bloodgroup:"O-ve",address :"hyderabad"},
+    {name :"lakshmi",age:22,bloodgroup:"b-ve",address:"bandar"}
+
+]
+console.log(student);
+console.log(student[2].name)
+console.log(student[1].address)

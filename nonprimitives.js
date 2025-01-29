0eae6e8bdcc3981bfbbe77f9f16b4fa7dca94d2b
Lakshmi2023-24 <lakshmisivakumari14398@gmail.com>
2025-01-29 10:20:51 +0530

    hey.js

---
+++ nonprimitives.js
@@ -0,0 +1,18 @@
+const movies = [
+"sankrantiki vasthunam",
+"dakkumaharaj",
+"gamechanger",
+"pushpa-2",
+"devara",
+"murari",]
+console.log("movies:",movies)
+console.log("movie:",movies.length)
+const moviesdetails = {
+title:"sankranthiki vasthunam",
+director:"anilravipudi",
+year:2025
+,Price:{normal:200,
+    premium:350,},
+showtimings:["10:00 a.m","2:00 p.m","6.00 p.m"]
+}
+console.log("moviesdetails:",moviesdetails)
